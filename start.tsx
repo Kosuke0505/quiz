be325d953fd276ac757c00dddb32d8d8ac0c9f55
Kosuke0505 <hwjfn0064@yahoo.co.jp>
2021-02-10 23:15:04 +0900

    変更1

--- start.tsx
+++ start.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { HeaderBackground } from 'react-navigation-stack';
 
 export default class App extends React.Component {
 	render() {
 		return (
             <View style={{backgroundColor:'yellow', height:'100%'}}>
                 <View style={styles.container}>
-                <Text>クイズゲーム</Text>
-                <Button
-                    title="start"
-                    onPress={() => {this.goquestion();}}>
-                </Button>
+                <Text style={styles.title}>ごみげーむ</Text>
+                    <View style={{backgroundColor:'yellow'}}>
+                        <Button
+                            title="start"
+                            onPress={() => {this.goquestion();}}>
+                        </Button>
+                    </View>
                 </View>
             </View>
 			);
@@ -29,4 +32,10 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         height: '100%',
     },
+    title: {
+        backgroundColor:'yellow',
+        fontSize:50,
+        fontFamily:"Arial",
+    }
+
   });
