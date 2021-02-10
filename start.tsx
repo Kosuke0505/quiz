1a71cef789781124476f46f48800310cc09b9ede
Y-koga0205 <kogayasunori0205@icloud.com>
2021-02-10 23:37:00 +0900

    テスト

--- start.tsx
+++ start.tsx
@@ -7,7 +7,7 @@ export default class App extends React.Component {
 		return (
             <View style={{backgroundColor:'yellow', height:'100%'}}>
                 <View style={styles.container}>
-                <Text style={styles.title}>ちんげーむ</Text>
+                <Text style={styles.title}>げーむ</Text>
                     <View style={{backgroundColor:'yellow'}}>
                         <Button
                             title="start"
